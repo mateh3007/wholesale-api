1d9095152188e6a8506a7e242bff37afcb600c5a
mateh3007 <mateh11233@gmail.com>
2023-05-30 02:21:51 +0100

    chore: created modules file and installed prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ModulesModule } from './modules/modules.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ModulesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
